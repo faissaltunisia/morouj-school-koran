3ca4c50f8732111649cb73bfd85f3518e5003dca
SalalahSchool <aribifaissal@gmail.com>
2025-12-22 20:22:00 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,33 @@
-const CACHE_NAME = 'murooj-v1';
-const assets = ['/', '/index.html'];
+const CACHE_NAME = 'murooj-v2';
+const assets = [
+  './',
+  './index.html',
+  'https://fonts.googleapis.com/css2?family=Amiri:wght@700&family=Tajawal:wght@300;500;800&display=swap'
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+// تثبيت ملفات الكاش الأساسية
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+// تفعيل المحرك ومسح الكاش القديم
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
+    })
+  );
+});
+
+// إدارة الطلبات (توفير السرعة)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
